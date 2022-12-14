5d9ebf5b7e900bbc209cf4fa3a9cb3f07f0f6424
JRisJunior <jeffmrubin@gmail.com>
2022-12-14 14:01:54 -0600

    add current user auth + start dogs create route

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,11 @@
+class DogsController < ApplicationController
+
+  def create
+    dog = Dog.new(
+      name: params[:name],
+      age: params[:age],
+      breed: params[:breed]
+    )
+    dog.save
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post "/dogs" => "dogs#create"
+
+  post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 end
